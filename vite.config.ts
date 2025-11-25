ab85a1506732fda222de9c5c824477cae30a5f7d
FilipeCoval <filipe_coval_90@hotmail.com>
2025-11-25 21:57:37 +0100

    build options

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', 
   build: {
     outDir: 'dist',
-    sourcemap: false,
-    target: 'esnext'
-  },
-  // Removemos 'define: { process.env: {} }' pois pode interferir com a deteção de ambiente em alguns casos
-  // O Vite trata automaticamente env vars que começam com VITE_
+    emptyOutDir: true,
+    sourcemap: false
+  }
 });
