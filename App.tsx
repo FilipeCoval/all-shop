c3b1358ad9c1dea52c7ef3ba7f8780aa81a40777
FilipeCoval <filipe_coval_90@hotmail.com>
2025-12-20 12:41:45 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -314,6 +314,10 @@ const App: React.FC = () => {
                     <div className="bg-white p-0.5 rounded h-8 w-12 flex items-center justify-center shadow-sm" title="Mastercard">
                         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Mastercard-logo.svg/200px-Mastercard-logo.svg.png" alt="Mastercard" className="h-full w-full object-contain" />
                     </div>
+                    {/* NOVO MÉTODO: COBRANÇA */}
+                    <div className="bg-white p-0.5 rounded h-8 w-12 flex items-center justify-center shadow-sm" title="Pagamento na Entrega">
+                        <img src="https://img.icons8.com/color/48/cash-on-delivery.png" alt="Cobrança" className="h-full w-full object-contain" />
+                    </div>
                 </div>
             </div>
         </div>
