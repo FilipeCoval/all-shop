2c7edb051aee017c8b170ec460d35664af6d4a0d
FilipeCoval <filipe_coval_90@hotmail.com>
2026-01-12 20:29:22 +0100

    Update og.ts

--- api/og.ts
+++ api/og.ts
@@ -1,4 +1,4 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 
