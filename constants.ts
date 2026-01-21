ab14e63868bcd07e2ac75a736e50f937e5132f7a
FilipeCoval <filipe_coval_90@hotmail.com>
2026-01-21 20:46:20 +0100

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -5,7 +5,7 @@ export const CURRENCY = "EUR";
 export const LOGO_URL = "https://i.imgur.com/nSiZKBf.png"; 
 
 // URL OFICIAL DA LOJA
-// Atualizado para o domínio personalizado
+// Atualizado para o domínio oficial com www
 export const PUBLIC_URL = "https://www.all-shop.net";
 
 export const SELLER_PHONE = "351933865907"; 
